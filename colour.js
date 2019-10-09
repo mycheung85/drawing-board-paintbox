232820fbbb54cd9942e41cf0fdaeeb9980e011da
mycheung85 <mycheung85@gmail.com>
2019-10-09 11:56:28 +0100

    added an event listener to select the colour

--- colour.js
+++ colour.js
@@ -1,27 +1,24 @@
-function pickColour() {
-  let swatches = document.querySelectorAll("swatch");
+let colourSwatch = document.querySelectorAll(".swatch");
+
+for (const colour of colourSwatch) {
+  colour.addEventListener("click", e => {
+    setSwatch(e);
+  });
+  false;
 }
 
-document.addEventListener("click", e => {
-  // console.log(e.target);
-  if (e.target.matches("swatch")) {
-    // swatches.classList.remove("active");
-    // e.target.classList.toggle("active", true);
-    console.log("hi");
+function setColour(colour) {
+  ctx.fillStyle = colour;
+  ctx.strokeStyle = colour;
+  let active = document.getElementsByClassName("active")[0];
+  if (active) {
+    active.className = "swatch";
   }
-  false;
-});
+}
 
-// console.log(inner.length);
-
-// function setColour(colour) {
-//   ctx.fillStyle = colour;
-//   ctx.strokeStyle = colour;
-// }
-
-// function setSwatch(e) {
-//   let target = e.target;
-//   target.classList.toggle("active", true);
-//   setColour(target.style.backgroundColor);
-//   // swatches.removeAtribute.classList("active");
-// }
+function setSwatch(e) {
+  let swatch = e.target;
+  setColour(swatch.style.backgroundColor);
+  // console.log("colour");
+  swatch.classList.toggle("active");
+}
